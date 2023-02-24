74f4e92bf97d50277de97f312aa0993df8639103
Alef Ojeda de Oliveira <alef.oliveira@dimensa.com.br>
2023-02-24 15:55:35 -0300

    fix error in load tasks

--- lib/database_backup/tasks.rb
+++ lib/database_backup/tasks.rb
@@ -4,8 +4,10 @@ module DatabaseBackup
   # Railtie to load rake tasks
   # Description: This class is responsible for loading rake tasks
   class Tasks < Rails::Railtie
+    railtie_name :database_backup
+
     rake_tasks do
-      load "database_backup/tasks/database_backup.rake"
+      Dir[File.join(File.dirname(__FILE__), '../tasks/*.rake')].each {|f| load f }
     end
   end
 end
